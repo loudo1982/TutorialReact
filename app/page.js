fca036d48a1108ad9f847d62666d793c2a853cfb
Ludovic Guillaume Florent Betton <ludovic@tec.mx>
2024-01-31 14:09:22 -0700

    fin

--- app/page.js
+++ app/page.js
@@ -1,14 +1,44 @@
 "use client"
-import Miboton from "./component/boton";
+import { useRouter } from 'next/navigation';
+import { useState } from 'react';
+
 
 export default function Home() {
+  const router = useRouter();
+  const [menuVisible, setMenuVisible] = useState(false);
 
-  const losnombres=['Ludo','Juan','Felipe','María','Juan']
+  const handleButtonClick = (pagina) => {
+    router.push(`/pages/${pagina}`);
+  };
+
+  const toggleMenuVisibility = () => {
+    setMenuVisible(!menuVisible);
+  };
+
+  const paginas = ['botones', 'formulario', 'todolist', 'fetchdata', 'charts', 'chartscountries'];
 
   return (
-    <div>
-     Página Principal
+    <div className="container mx-auto my-8 text-center">
+      <h1 className="text-4xl font-bold mb-6">Página Principal</h1>
+      <button
+        onClick={toggleMenuVisibility}
+        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mb-4"
+      >
+        {menuVisible ? 'Ocultar Menú' : 'Mostrar Menú'}
+      </button>
+      {menuVisible && (
+        <div className="grid grid-cols-2 gap-4">
+          {paginas.map((pagina, index) => (
+            <button
+              key={index}
+              onClick={() => handleButtonClick(pagina)}
+              className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+            >
+              Ir a {pagina}
+            </button>
+          ))}
+        </div>
+      )}
     </div>
-   
   );
 }
